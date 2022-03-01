45be1219947f3dcb453c45ebc33f2e4055abf200
Builditluc <37375448+Builditluc@users.noreply.github.com>
2022-03-01 20:01:23 +0100

    remove old nix configuration

--- .config/nixpkgs/colors.nix
+++
@@ -1,7 +0,0 @@
-let
-  nix-colors = builtins.fetchTarball "https://github.com/misterio77/nix-colors/archive/main.tar.gz"; 
-in
-{
-  colorscheme = (import "${nix-colors}/schemes").nord;
-  colorscheme-name = "nord";
-}
