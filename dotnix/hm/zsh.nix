2ccc775fba08a5fa67ab80f9fd42059db06fc0af
Builditluc <37375448+Builditluc@users.noreply.github.com>
2022-07-16 17:47:51 +0200

    add clipboard function to zsh

--- dotnix/hm/zsh.nix
+++ dotnix/hm/zsh.nix
@@ -12,6 +12,28 @@
     };
     initExtraFirst = ''
       auto-ls-git-onefetch () { if (ls -laF | grep .git/ >> /dev/null); then onefetch; fi; }
+      pss () {
+        folder=$(pwd)
+        filename="Screen Shot $(date +%Y-%m-%d\ at\ %H.%M.%S).png"
+
+        if [ $# -ne 0 ]; then
+            if [[ -d $1 ]]; then
+                if [ "$1" != "." ]; then folder=$1; fi
+            else
+                a=$(dirname "$1")
+                b=$(basename "$1" .png)
+
+                if [ "$b" != "" ]; then filename=$b.png; fi
+
+                if [ "$a" != "." ]; then folder=$a; fi
+            fi
+        fi
+
+        osascript -e "tell application \"System Events\" to ¬
+                write (the clipboard as «class PNGf») to ¬
+                (make new file at folder \"$folder\" ¬
+                with properties {name:\"$filename\"})"
+    }
     '';
     initExtra = ''
       source /etc/static/zshrc
